1fc75e191629b478d25115105eadfefbb6548bfe
mharis001 <mhariszakar@gmail.com>
2018-09-07 18:34:37 -0400

    Change waypoint icons to 3DEN ones

---
+++ addons/common/CfgCurator.hpp
@@ -0,0 +1,14 @@
+class CfgCurator {
+    class DrawWaypoint {
+        class 3D {
+            texture = "\a3\3den\data\cfgwaypoints\move_ca.paa";
+            textureCycle = "\a3\3den\data\cfgwaypoints\cycle_ca.paa";
+            texturePreview = "\a3\3den\data\cfgwaypoints\move_ca.paa";
+        };
+        class 2D {
+            texture = "\a3\3den\data\cfgwaypoints\move_ca.paa";
+            textureCycle = "\a3\3den\data\cfgwaypoints\cycle_ca.paa";
+            texturePreview = "\a3\3den\data\cfgwaypoints\move_ca.paa";
+        };
+    };
+};
