d9f9ee7ed072d6c84e5bcd9b66c02f15a19075f0
mharis001 <mhariszakar@gmail.com>
2018-10-11 23:11:11 -0400

    Just change file in CfgFunctions

--- addons/attributes/CfgFunctions.hpp
+++ addons/attributes/CfgFunctions.hpp
@@ -1,9 +1,6 @@
 class CfgFunctions {
     class A3_Functions_F_Curator {
         class Curator {
-            delete showCuratorAttributes;
-        };
-        class ZEN {
             class showCuratorAttributes {
                 file = QPATHTOF(functions\fnc_bi_showCuratorAttributes.sqf);
             };
