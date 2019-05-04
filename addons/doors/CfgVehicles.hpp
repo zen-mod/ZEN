35b7eb61f41da37bd41cb314fae7cfa762e13c33
mharis001 <mhariszakar@gmail.com>
2019-05-03 21:10:10 -0400

    Change icon for configure doors module

--- addons/doors/CfgVehicles.hpp
+++ addons/doors/CfgVehicles.hpp
@@ -5,7 +5,7 @@ class CfgVehicles {
         curatorCanAttach = 1;
         category = QEGVAR(modules,Buildings);
         displayName = CSTRING(Configure);
-        icon = "\a3\modules_f\data\editterrainobject\texturedoor_opened_ca.paa";
+        icon = "\a3\ui_f\data\igui\cfg\actions\open_door_ca.paa";
         function = QFUNC(module);
     };
 };
