24ab14aa7ff6258e9fee5f99797a2e395ef4838b
Ralfs Garkaklis <ralfs@garkaklis.com>
2019-08-08 20:20:48 +0300

    Added music component

---
+++ addons/music/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT music
+#define COMPONENT_BEAUTIFIED Music
+#include "\x\zen\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_MARKERS_TREE
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_MARKERS_TREE
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_MARKERS_TREE
+#endif
+
+#include "\x\zen\addons\main\script_macros.hpp"
