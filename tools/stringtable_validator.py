5a2ef97ec3c73352f00f21d446a920693f00a836
mharis001 <mhariszakar@gmail.com>
2019-11-09 00:53:47 -0500

    Skip checks if package is None

--- tools/stringtable_validator.py
+++ tools/stringtable_validator.py
@@ -46,74 +46,74 @@ def check_stringtable(filepath):
     if package is None:
         print("  ERROR: Failed to find 'Package' tag under 'Project' tag.")
         errors += 1
+    else:
+        package_name = package.get("name")
 
-    package_name = package.get("name")
-
-    if package_name.islower():
-        print("  ERROR: Package name attribute '{}' is all lowercase, should be in titlecase.".format(package_name))
-        errors += 1
-
-    if package_name.lower() != os.path.basename(os.path.dirname(filepath)):
-        print("  ERROR: Package name attribute '{}' does not match the component folder name.".format(package_name))
-        errors += 1
-
-    # Get all keys contained in the stringtable
-    keys = package.findall("Key")
-
-    for container in package.findall("Container"):
-        keys.extend(container.findall("Key"))
-
-    key_ids = []
-    key_prefix = "STR_{}_{}_".format(PROJECT_NAME, package_name)
-
-    for key in keys:
-        key_id = key.get("ID")
-
-        # Verify that the key has a valid ID attribute
-        if key_id is None:
-            print("  ERROR: Key '{}' had no ID attribute.".format(key_id))
-            errors += 1
-        elif key_id.find(key_prefix) != 0:
-            print("  ERROR: Key '{}' does not have a valid ID attribute, should be in format {}{{name}}.".format(key_id, key_prefix))
+        if package_name.islower():
+            print("  ERROR: Package name attribute '{}' is all lowercase, should be in titlecase.".format(package_name))
             errors += 1
 
-        key_ids.append(key_id)
-
-        # Verify language entries for the key, check that the key:
-        #   - contains at least one translation
-        #   - does not contain an Original translation
-        #   - has English as the first listed translation
-        #   - has only one entry for each language
-        entries = list(key)
-
-        if len(entries) == 0:
-            print("  ERROR: Key '{}' has no translation entries.".format(key_id))
+        if package_name.lower() != os.path.basename(os.path.dirname(filepath)):
+            print("  ERROR: Package name attribute '{}' does not match the component folder name.".format(package_name))
             errors += 1
-        else:
-            if not key.find("Original") is None:
-                print("  ERROR: Key '{}' has an Original translation, unnecessary with English as first.".format(key_id))
+
+        # Get all keys contained in the stringtable
+        keys = package.findall("Key")
+
+        for container in package.findall("Container"):
+            keys.extend(container.findall("Key"))
+
+        key_ids = []
+        key_prefix = "STR_{}_{}_".format(PROJECT_NAME, package_name)
+
+        for key in keys:
+            key_id = key.get("ID")
+
+            # Verify that the key has a valid ID attribute
+            if key_id is None:
+                print("  ERROR: Key '{}' had no ID attribute.".format(key_id))
+                errors += 1
+            elif key_id.find(key_prefix) != 0:
+                print("  ERROR: Key '{}' does not have a valid ID attribute, should be in format {}{{name}}.".format(key_id, key_prefix))
                 errors += 1
 
-            if entries[0].tag != "English":
-                print("  ERROR: Key '{}' does not have its English translation listed first.".format(key_id))
+            key_ids.append(key_id)
+
+            # Verify language entries for the key, check that the key:
+            #   - contains at least one translation
+            #   - does not contain an Original translation
+            #   - has English as the first listed translation
+            #   - has only one entry for each language
+            entries = list(key)
+
+            if len(entries) == 0:
+                print("  ERROR: Key '{}' has no translation entries.".format(key_id))
                 errors += 1
-
-            languages = list(map(lambda l: l.tag, entries))
-
-            for language in set(languages):
-                count = languages.count(language)
-
-                if count > 1:
-                    print("  ERROR: Key '{}' has {} {} translations.".format(key_id, count, language))
+            else:
+                if not key.find("Original") is None:
+                    print("  ERROR: Key '{}' has an Original translation, unnecessary with English as first.".format(key_id))
                     errors += 1
 
-    # Verify that key IDs are unique
-    for id in set(key_ids):
-        count = key_ids.count(id)
+                if entries[0].tag != "English":
+                    print("  ERROR: Key '{}' does not have its English translation listed first.".format(key_id))
+                    errors += 1
 
-        if count > 1:
-            print("  ERROR: Key '{}' is duplicated {} time(s).".format(id, count))
-            errors += 1
+                languages = list(map(lambda l: l.tag, entries))
+
+                for language in set(languages):
+                    count = languages.count(language)
+
+                    if count > 1:
+                        print("  ERROR: Key '{}' has {} {} translations.".format(key_id, count, language))
+                        errors += 1
+
+        # Verify that key IDs are unique
+        for id in set(key_ids):
+            count = key_ids.count(id)
+
+            if count > 1:
+                print("  ERROR: Key '{}' is duplicated {} time(s).".format(id, count))
+                errors += 1
 
     if errors == 0:
         print("No errors found.")
