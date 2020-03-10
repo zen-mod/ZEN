dfed1b476b66a3330d1655cd7246aaec31212a19
Ralfs Garkaklis <ralfs@garkaklis.com>
2020-03-10 15:35:47 +0200

    Fixed wrong binaries being downloaded

--- tools/getHemtt.py
+++ tools/getHemtt.py
@@ -6,58 +6,80 @@ import os
 import os.path as path
 import zipfile
 import tarfile
+import sys
 
-print('Fetching API data')
 
-with urllib.request.urlopen("https://api.github.com/repos/synixebrett/HEMTT/releases/latest") as url:
-    releaseJson = json.loads(url.read().decode())['assets']
+def find_release_json(releaseJson, system):
+    for release in releaseJson:
+        name = release['name']
 
-    scriptDir = path.dirname(path.realpath(__file__))
-    oneDirUp = path.abspath(path.join(scriptDir, '..'))
+        if (system in name):
+            return release
 
-    hemttFile = path.join(oneDirUp, 'hemtt.exe')
-    extension = '.zip'
 
-    if (platform.system() == 'Windows'):
-        releaseJson = releaseJson[3]
-    elif (platform.system() == 'Linux'):
-        releaseJson = releaseJson[4]
+def main():
+    print('Fetching API data')
+
+    with urllib.request.urlopen("https://api.github.com/repos/synixebrett/HEMTT/releases/latest") as url:
+        releaseJson = json.loads(url.read().decode())['assets']
+
+        scriptDir = path.dirname(path.realpath(__file__))
+        oneDirUp = path.abspath(path.join(scriptDir, '..'))
+
         hemttFile = path.join(oneDirUp, 'hemtt')
         extension = '.tar.gz'
-    else:
-        raise OSError('Your OS is not supported by this utility. Download HEMTT manually.')
 
-    releaseUrl = releaseJson['browser_download_url']
+        if (platform.system() == 'Windows'):
+            hemttFile = path.join(oneDirUp, 'hemtt.exe')
+            extension = '.zip'
 
-    zipPath = path.join(oneDirUp, 'hemtt{}'.format(extension))
+            releaseJson = find_release_json(
+                releaseJson, 'x86_64-pc-windows-msvc.zip')
+        elif (platform.system() == 'Linux'):
+            releaseJson = find_release_json(
+                releaseJson, 'x86_64-unknown-linux-gnu.tar.gz')
+        elif (platform.system() == 'Darwin'):
+            releaseJson = find_release_json(
+                releaseJson, 'x86_64-apple-darwin.tar.gz')
+        else:
+            raise OSError(
+                'Your OS is not supported by this utility. Download HEMTT manually.')
 
-    if (path.isfile(hemttFile)):
-        print('Removing existing HEMTT version')
-        os.remove(hemttFile)
+        releaseUrl = releaseJson['browser_download_url']
 
-    print('Downloading from {}'.format(releaseUrl))
-    urllib.request.urlretrieve(releaseUrl, zipPath)
+        zipPath = path.join(oneDirUp, 'hemtt{}'.format(extension))
 
-    print('Extracting archive')
+        if (path.isfile(hemttFile)):
+            print('Removing existing HEMTT version')
+            os.remove(hemttFile)
 
-    extractPath = path.join(oneDirUp, 'tmp')
-    if (extension == '.zip'):
-        hemtt = zipfile.ZipFile(zipPath, 'r')
-        hemtt.extractall(extractPath)
-        hemtt.close()
+        print('Downloading from {}'.format(releaseUrl))
+        urllib.request.urlretrieve(releaseUrl, zipPath)
 
-        print('Moving files')
-        os.remove(path.join(extractPath, 'setup.exe'))
-        os.rename(path.join(extractPath, 'hemtt.exe'), hemttFile)
-        os.removedirs(extractPath)
-    else:
-        hemtt = tarfile.open(zipPath, 'r')
-        hemtt.extractall(extractPath)
-        hemtt.close()
+        print('Extracting archive')
 
-        print('Moving files')
-        os.rename(path.join(extractPath, 'hemtt'), hemttFile)
-        os.removedirs(extractPath)
+        extractPath = path.join(oneDirUp, 'tmp')
+        if (extension == '.zip'):
+            hemtt = zipfile.ZipFile(zipPath, 'r')
+            hemtt.extractall(extractPath)
+            hemtt.close()
 
-    print('Removing temp archive file')
-    os.remove(zipPath)
+            print('Moving files')
+            os.remove(path.join(extractPath, 'setup.exe'))
+            os.rename(path.join(extractPath, 'hemtt.exe'), hemttFile)
+            os.removedirs(extractPath)
+        else:
+            hemtt = tarfile.open(zipPath, 'r')
+            hemtt.extractall(extractPath)
+            hemtt.close()
+
+            print('Moving files')
+            os.rename(path.join(extractPath, 'hemtt'), hemttFile)
+            os.removedirs(extractPath)
+
+        print('Removing temp archive file')
+        os.remove(zipPath)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
