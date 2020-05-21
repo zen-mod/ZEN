8ad6528ad40a3def3df4daf4037f1179c3295422
oOKexOo <rc_mapper@yahoo.com>
2020-05-21 18:10:25 +0200

    Move duplicate blocks to preprocessor

--- addons/characters/script_component.hpp
+++ addons/characters/script_component.hpp
@@ -15,3 +15,21 @@
 #endif
 
 #include "\x\zen\addons\main\script_macros.hpp"
+
+#define C_MAN_SICK_ATTRIBUTE_MODIFICATIONS \
+    editorSubcategory = "EdSubcat_Personnel_Sick";\
+    scope = 2;\
+    scopeCurator = 2;\
+    class EventHandlers {\
+        init = "(_this select 0) setIdentity selectRandom [\
+            'BIS_Ambient01_sick',\
+            'BIS_Ambient02_sick',\
+            'BIS_Ambient03_sick',\
+            'BIS_Arthur_Sick',\
+            'BIS_Howard_sick',\
+            'BIS_John_sick',\
+            'BIS_Lucas_sick',\
+            'BIS_Renly_sick'\
+        ];\
+        (_this select 0) setDamage 0.45";\
+    };
