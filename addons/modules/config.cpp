76120e62dee45513b659277dc1036d23ff75eaa2
ampersand38 <ampersand38@gmail.com>
2020-06-24 10:22:52 -0400

    Revert "Update config.cpp"
    This reverts commit 07aefb106cf8c2ed07138f7263351598004fd585.

--- addons/modules/config.cpp
+++ addons/modules/config.cpp
@@ -78,7 +78,7 @@ class CfgPatches {
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {"zen_attributes"};
         author = ECSTRING(main,Author);
-        authors[] = {"mharis001"};
+        authors[] = {"mharis001", "Ampersand"};
         url = ECSTRING(main,URL);
         VERSION_CONFIG;
     };
