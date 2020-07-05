a4c5ce2edbee9413c96f3c3eaf453dd18e8b1fbc
ampersand38 <ampersand38@gmail.com>
2020-07-05 00:52:25 -0400

    remove testing

--- addons/modules/XEH_PREP.hpp
+++ addons/modules/XEH_PREP.hpp
@@ -1,6 +1,3 @@
-// testing, update fncs on the fly
-#define PREP(var1) TRIPLES(ADDON,fnc,var1) = { call compile preProcessFileLineNumbers '\MAINPREFIX\PREFIX\SUBPREFIX\COMPONENT_F\functions\DOUBLES(fnc,var1).sqf' }
-
 PREP(addIntelAction);
 PREP(addTeleporterAction);
 PREP(bi_moduleCurator);
