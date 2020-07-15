980613dc6f41a1f19af4e7ff5402232fadf98d82
ampersand38 <ampersand38@gmail.com>
2020-07-14 22:21:09 -0400

    Moved to attributes component

--- addons/attributes/script_component.hpp
+++ addons/attributes/script_component.hpp
@@ -17,6 +17,7 @@
 #include "\x\zen\addons\main\script_macros.hpp"
 
 #include "\a3\ui_f\hpp\defineCommonGrids.inc"
+#include "\a3\ui_f\hpp\defineDIKCodes.inc"
 #include "\x\zen\addons\common\defineResinclDesign.inc"
 
 #define POS_X(N) ((N) * GUI_GRID_W + GUI_GRID_CENTER_X)
