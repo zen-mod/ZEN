e4879af208bc3c3008d303e6525a37de2c666a9d
mharis001 <mhariszakar@gmail.com>
2021-01-11 22:37:39 -0500

    Add Faction Filter

---
+++ addons/faction_filter/script_component.hpp
@@ -0,0 +1,21 @@
+#define COMPONENT faction_filter
+#define COMPONENT_BEAUTIFIED Faction Filter
+#include "\x\zen\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_FACTION_FILTER
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_FACTION_FILTER
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_FACTION_FILTER
+#endif
+
+#include "\x\zen\addons\main\script_macros.hpp"
+
+#include "\x\zen\addons\common\defineResinclDesign.inc"
+
+#define FACTION_ID(side,name) format [QGVAR(%1:%2), side, name]
