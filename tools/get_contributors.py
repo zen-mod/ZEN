adf2955f3e5dd8d8edfa85cba20c75cf348212eb
Kex <kexanone@gmail.com>
2021-08-19 01:05:16 +0200

    Get core team from file

--- tools/get_contributors.py
+++ tools/get_contributors.py
@@ -6,29 +6,31 @@ import urllib.request
 
 tools_path = Path(__file__).parent
 
-authors = set([
-    'ampersand38',
-    'CreepPork',
-    'Kexanone',
-    'mharis001',
-    'neilzar'
-])
+core_team = set()
+contributors = set()
 
 # Get contributors from GitHub
 result = urllib.request.urlopen('http://api.github.com/repos/zen-mod/ZEN/contributors')
 body = result.read()
-contributors = set()
 for entry in json.loads(body.decode("utf-8")):
     contributors.add(entry['login'])
 
-# Get other contributors
+# Get core_team and other contributors
 with open(tools_path / '../AUTHORS.txt', 'r') as stream:
+    for line in stream:
+        if line.startswith('# CORE TEAM'):
+            break
+    for line in stream:
+        try:
+            core_team.add(line.split()[0])
+        except IndexError:
+            break
     for line in stream:
         if line.startswith('# CONTRIBUTORS'):
             break
     for line in stream:
         contributors.add(line.strip())
 
-contributors -= authors
+contributors -= core_team
 
 print('\n'.join(sorted(contributors, key=lambda name: name.lower())))
