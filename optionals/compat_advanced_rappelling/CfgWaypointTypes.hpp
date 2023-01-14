0a51be7348ef19cd27b38a0808d1f166fec90c03
Kex <kexanone@gmail.com>
2023-01-14 02:21:29 +0100

    Remove unchanged config entries

--- optionals/compat_advanced_rappelling/CfgWaypointTypes.hpp
+++ optionals/compat_advanced_rappelling/CfgWaypointTypes.hpp
@@ -1,7 +1,5 @@
 class ZEN_WaypointTypes {
     class Fastrope {
-        displayName = ECSTRING(ai,Fastrope);
-        type = "SCRIPTED";
         script = QPATHTOEF(compat_advanced_rappelling,functions\fnc_waypointFastrope.sqf);
     };
 };
