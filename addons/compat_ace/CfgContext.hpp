e0336bbf8af2700067cbda916e5c4aed37f419bc
OverlordZorn <dennisAzorn@gmail.com>
2025-10-17 14:54:51 +0200

    string pascalcase

--- addons/compat_ace/CfgContext.hpp
+++ addons/compat_ace/CfgContext.hpp
@@ -1,6 +1,6 @@
 class zen_context_menu_actions {
     class ace_medical_menu {
-        displayName = CSTRING(openMedicalMenu);
+        displayName = CSTRING(OpenMedicalMenu);
         icon = QPATHTOEF(context_actions,ui\medical_cross_ca.paa);
         condition = QUOTE([_hoveredEntity] call FUNC(canOpenMedicalMenu));
         statement = QUOTE([_hoveredEntity] call FUNC(openMedicalMenu));
