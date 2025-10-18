db2a4a7309dfffd14a065550c5caa5ef8773b40e
OverlordZorn <dennisAzorn@gmail.com>
2025-10-19 01:42:07 +0200

    Update CfgContext.hpp

--- addons/compat_ace/CfgContext.hpp
+++ addons/compat_ace/CfgContext.hpp
@@ -1,9 +1,9 @@
-class zen_context_menu_actions {
-    class ace_medical_menu {
+class EGVAR(context_menu,actions) {
+    class AceMedicalMenu {
         displayName = CSTRING(OpenMedicalMenu);
         icon = QPATHTOEF(context_actions,ui\medical_cross_ca.paa);
-        condition = QUOTE([_hoveredEntity] call FUNC(canOpenMedicalMenu));
-        statement = QUOTE([_hoveredEntity] call FUNC(openMedicalMenu));
+        condition = QUOTE(_hoveredEntity call FUNC(canOpenMedicalMenu));
+        statement = QUOTE(_hoveredEntity call FUNC(openMedicalMenu));
         priority = 51;
     };  
 };
